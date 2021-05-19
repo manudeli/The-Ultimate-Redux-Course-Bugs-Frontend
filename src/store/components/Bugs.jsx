a71447db9d149f1948b0f15e53bf86f3a817d73e
flowithus <flowithusdev@gmail.com>
2021-05-19 18:37:51 +0900

    Subscribing and Dispatching without react-redux

--- src/store/components/Bugs.jsx
+++ src/store/components/Bugs.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import StoreContext from "../contexts/storeContext";
-
+import bugs, { loadBugs } from "../bugs";
 export default class Bugs extends Component {
   static contextType = StoreContext;
 
+  state = { bugs: [] };
+
   componentDidMount() {
-    console.log(this.context);
+    const store = this.context;
+
+    this.unsubscribe = store.subscribe(() => {
+      const bugsInStore = store.getState().entities.bugs.list;
+      this.setState({ bugs: bugsInStore });
+    });
+
+    store.dispatch(loadBugs());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
-    return <div>Bugs</div>;
+    return (
+      <ul>
+        {this.state.bugs.map((bug) => (
+          <li key={bug.id}>{bug.description}</li>
+        ))}
+      </ul>
+    );
   }
 }
